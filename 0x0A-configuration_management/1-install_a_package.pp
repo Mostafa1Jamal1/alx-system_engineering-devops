d954ac0c70de9a3e37376c324f387ac0b109ab50
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2023-12-23 04:21:17 +0200

    fix: flask version dependancy

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,11 @@
 # install flask from pip3 Version 2.1.0
+
+# Werkzeug version 2.1.1 must be installed
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
