ee35cf44c9f8c47d1b4a589655609832a3e521f6
Mostafa <mostafa.jamal.mjm@gmail.com>
2024-02-19 22:24:15 +0200

    style: pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,17 +8,21 @@ from sys import argv
 
 if __name__ == "__main__":
     employee_id = argv[1]
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}?_embed=todos'
+    url = "https://jsonplaceholder.typicode.com/users/{}?_embed=todos".format(
+        employee_id)
     r = requests.get(url)
     respond_dict = json.loads(r.text)
-    
+
     name = respond_dict.get('name')
     todos = respond_dict.get('todos')
     completed = []
     for task in todos:
         if task.get('completed'):
             completed.append(task.get('title'))
-    
-    print(f"Employee {name} is done with tasks({len(completed)}/{len(todos)}):")
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        name, len(completed), len(todos)
+    ))
+
     for task in completed:
         print(f"\t {task}")
