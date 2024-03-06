899a815087a79c9f867af87f61a79865f793c816
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-03-06 04:15:45 +0200

    empty files

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' queries the Reddit API and returns the number of subscribers
+for a given subreddit.
+If an invalid subreddit is given, the function should return 0.'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    ''' for a given subreddit returns the number of subscribers '''
+
