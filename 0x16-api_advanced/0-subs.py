3f209e26f59e9e443a33eb80533448252dffdadf
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-03-07 09:12:08 +0200

    a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,8 +3,14 @@
 for a given subreddit.
 If an invalid subreddit is given, the function should return 0.'''
 import requests
+from uuid import uuid4
 
 
 def number_of_subscribers(subreddit):
     ''' for a given subreddit returns the number of subscribers '''
-
+    headers = {'user-agent': 'Mozilla/5.0 AppleWebKit/537.36 holberton school'}
+    url = f'https://www.reddit.com/r/{subreddit}/about/.json'
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code != 200:
+        return 0
+    return r.json().get('data').get('subscribers')
