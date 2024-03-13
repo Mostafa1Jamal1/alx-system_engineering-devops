ae3fe32322e234a1ae685a6d3b36fc639898cdd2
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-03-13 12:15:27 +0200

    remove open file limit of the holberton user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # remove the limit of open files for holberton user
 
 exec { 'remove limit':
-  command => 'sed -i "/holberton/d" /etc/security/limits.conf'
+  command => '/bin/sed -i "/holberton/d" /etc/security/limits.conf'
 }
