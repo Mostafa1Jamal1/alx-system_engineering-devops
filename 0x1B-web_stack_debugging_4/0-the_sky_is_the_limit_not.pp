bce3b442fb53375840390d4a4d3f5b5d3a20b387
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-03-14 20:45:44 +0200

    fix: quotations

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # fix our stack so that we get to 0 failing requests
 
 exec { 'fix our stack':
-  command => '/bin/sed -i 's/worker_processes [1-5];/worker_processes 6;/' /etc/nginx/nginx.conf'
+  command => '/bin/sed -i "s/worker_processes [1-5];/worker_processes 6;/" /etc/nginx/nginx.conf'
 }
 
 exec { 'restart nginx':
